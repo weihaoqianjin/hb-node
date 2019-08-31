dd046d0693fc13dbae9c0bfe21bc7df674367842
huaban <2544239995@qq.com>
2019-08-31 19:21:31 +0800

    =模块地址拼接兼容linux

--- callback.js
+++ callback.js
@@ -1,9 +1,17 @@
 let config = require('./config')
 let getLogger = require('./log')
+let os = require('os')
 module.exports = (req, res, next) => {
     let baseUrl = req.baseUrl
-    let apiDir = __dirname + baseUrl.substr(0, baseUrl.lastIndexOf('/')).replace(/\//g, '\\')
-    let apiGfuncDir = apiDir.substring(0, apiDir.lastIndexOf('\\')) + '\\gfunc'
+    let apiDir = ''
+    let apiGfuncDir = ''
+    if (os.type().toLowerCase().includes('windows')) {
+        apiDir = __dirname + baseUrl.substr(0, baseUrl.lastIndexOf('/')).replace(/\//g, '\\')
+        apiGfuncDir = apiDir.substring(0, apiDir.lastIndexOf('\\')) + '\\gfunc'
+    } else {
+        apiDir = __dirname + baseUrl.substr(0, baseUrl.lastIndexOf('/'))
+        apiGfuncDir = apiDir.substring(0, apiDir.lastIndexOf('/')) + '/gfunc'
+    }
     let apiId = baseUrl.substr(baseUrl.lastIndexOf('/') + 1)
     // 文件热更新
     if (config.debug) {
